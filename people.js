f843cc955a7350d680b4488c2a91f045aae12c36
this-is-emon <mofijul.islam.emon@gmail.com>
2024-01-26 23:01:51 +0600

    Module Concept : IIFE function

--- people.js
+++ people.js
@@ -1,7 +1,26 @@
-const name = ['Sakib','Tamim','Himu','Misir Ali']
-const a = 5;
-function test() {
-    console.log('test');
-}
+//IIFE stands for Immediately Invoked Function Expression. It is a design pattern in JavaScript 
+//where a function is defined and executed immediately after being created. 
+// 'IIFE' function structure:
+/*
+(function() {
+  // This code will be executed immediately
+  var localVar = "Hello, IIFE!";
+  console.log(localVar);
+})();
 
-module.exports= {name, a , test}
+*/
+
+//It is said that all codes in  is wrapped by a “Module Wrapper Function(IIFE function)” in nod.Js
+
+(function (exports, require, module, __filename, __dirname) {
+    
+    const name = ['Sakib','Tamim','Himu','Misir Ali']
+    const a = 5;
+    function test() {
+        console.log('test');
+    }
+
+    module.exports= {name, a , test}
+    return module.exports;
+
+})();
